195286edceadf42a44af8f2e2c9a79bed78addaf
Max Zelarayan <maxzelarayan@gmail.com>
2018-01-09 21:43:01 -0300

    Saving progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
+import { routing } from './app.routing';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    FavoritesComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    { path: 'search', component: SearchComponent },
+    { path: 'favorites', component: FavoritesComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
